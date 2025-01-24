bd6c81b59ee2e7dc73710fc745b47d71151e3672
jamo89 <jameson89@live.com>
2025-01-24 13:17:03 -0800

    honestly....i don't even know really...tutorial was moving waaaaay to fast

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,40 @@ import "./App.css";
 import {useState} from "react";
 
 function App () {
-    const [name, setName] = useState("");
+    const [score, setScore] = useState("10");
+    const [comment, setComment] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setName("");
-        console.log("Form submitted!");
-    };
+        if (Number(score) <= 5 && comment.length <= 10) {
+            alert("fuck you mean less than 5? bro get the fuck outta here we don't want your business snobby ass");
+            return;
+        }
+
+        console.log("Form submitted");
+        setComment("");
+        setScore("10");
+    }
 
     return (
         <div className="App">
             <form onSubmit={handleSubmit}>
                 <fieldset>
+                    <h2>Feedback form</h2>
                     <div className="Field">
-                        <label htmlFor="name">Name</label>
+                        <label>Score: {score} </label>
                         <input
-                            id="name"
-                            type="text"
-                            placeholder="name"
-                            name="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
+                            type="range"
+                            min="0"
+                            max="10"
+                            value={score}
+                            onChange={e => setScore(e.target.value)}/>
                     </div>
-                    <button disabled={!name}type="submit">
-                        Submit
-                    </button>
+                    <div className="Field">
+                        <label>Comment:</label>
+                        <textarea value={comment} onChange={e => setComment(e.target.value)}/>
+                    </div>
+                    <button type="submit">Submit</button>
                 </fieldset>
             </form>
         </div>
