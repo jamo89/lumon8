a5185cd7c43c34ed1596d8e0953af71426083fbb
jamo89 <jameson89@live.com>
2025-01-27 12:43:29 -0800

    made a button that changes an h1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,19 @@
-import "./App.css";
-import { UserProvider,useUser } from "./UserContext";
-console.log("Hello World");
-const LoggedInUser = () => {
-    const {user} = useUser();
-    console.log("USER",user);
-    return (
-        <p>
-            Hello <span className="Username">{user.name}</span>
-        </p>
-    );
-};
+import {useState} from "react";
 
-const Header = () => {
-    console.log("header")
-    return (
-        <header>
-            Jamo
-            <h2> Blog App</h2>
-            <LoggedInUser />
-        </header>
-    );
-};
+export default function App() {
+    const [restaurantName, setRestaurantName] = useState("Lemon");
 
-const Page = () => {
-    const { user } = useUser();
-    console.log("page");
+    function updateRestaurantName() {
+        setRestaurantName("lil Lumon")
+    }
+
     return (
         <div>
-            hello
-            <h2>What it is Lorem</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eum ipsa itaque nesciunt nisi, quae! Adipisci cupiditate, iste modi molestiae mollitia nemo possimus quibusdam repellat, repellendus sit ullam, unde voluptates.
-            </p>
-            <p>Written by {user.name}</p>
+            <h1>{restaurantName}</h1>
+            <button onClick={updateRestaurantName}>
+                Update Restaurant Name
+            </button>
         </div>
     );
 };
 
-function App() {
-    console.log("App")
-    return(
-        <div className="App">
-            HELLO AGAIN
-            <Header />
-            <Page />
-        </div>
-    );
-}
-
-function Root() {
-    console.log("Root")
-    return (
-        <UserProvider>
-            <App />
-        </UserProvider>
-    );
-}
-
-export default Root;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,8 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      HELLO
-    <App />
+      <App />
   </React.StrictMode>
 );
 
