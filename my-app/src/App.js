e03a22c016ab84d3c38825408e4806c6e0a6e098
jamo89 <jameson89@live.com>
2025-01-30 11:32:58 -0800

    did a thing with react fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import './App.css';
+import React from 'react';
 
 function App() {
-    const [toggle, setToggle] = React.useState(false);
+    const [user, setUser] = React.useState([]);
 
-    const clickHandler = () => {
-        setToggle(!toggle);
+    const fetchData = () => {
+        fetch("https://randomuser.me/api/?results=1")
+            .then(response => response.json())
+            .then(data => setUser(data));
     }
 
     React.useEffect(() => {
-        document.title = toggle ? "Welcome to lil Lumon" : "Using the useEffect hook"
-    }, [toggle]);
+        fetchData();
+    },[]);
 
-    return (
+    return Object.keys(user).length > 0 ? (
         <div>
-            <h1>Using the useEffect hook</h1>
-            <button onClick={clickHandler}>
-                Toggle message
-            </button>
-            {toggle && <h2>Welcome to lil Lumon</h2>}
+            <h1>Data returned</h1>
+            <h2>First Name: {user.results[0].name.first}</h2>
+            <h2>Last Name: {user.results[0].name.last}</h2>
         </div>
-    )
+    ) : (
+        <h1>Data pending...</h1>
+    );
 }
 
 export default App;
