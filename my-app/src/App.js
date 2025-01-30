998fa99e85df797c81c0fb42f5d8dbeeceb33d4a
jamo89 <jameson89@live.com>
2025-01-30 12:34:10 -0800

    it appears that the module i was working on did not mention they were using something like bootstrap which would have leveraged lines 10,11 differently to create a pop up alert..

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
 import './App.css';
 
-function App() {
-    const formInputRef = React.useRef(null);
+const Button = ({children, backgroundColor}) =>{
+    return <button style={{ backgroundColor }}>{children}</button>
+};
 
-    const focusInput = () => {
-        formInputRef.current.focus();
-    }
-
+const Alert = ({children}) =>{
     return (
-      <>
-          <h1>Using useRef to access underlying DOM</h1>
-          <input ref={formInputRef} type="text"/>
-          <input type="text"/>
-          <button onClick={focusInput}>
-              Focus input
-          </button>
-      </>
+        <>
+        <div className="Overlay"/>
+        <div className="Alert">{children}</div>
+        </>
+    );
+};
+
+const DeleteButton =() => {
+    return <Button backgroundColor="red">Delete</Button>
+};
+
+function App() {
+    return (
+        <div className="App">
+            <header>Lil Lumon Header</header>
+            <Alert>
+                <h4>Delete Account</h4>
+                <p>
+                    can you believe JLAW shopped here once? and you want to delete your account on this lemon restuarant you went through the effort of making an account for? why don't they just have you sign in with your google account? what the hell man
+                </p>
+                <DeleteButton />
+            </Alert>
+        </div>
     );
 }
 
