bc7c37a64cbd75958b380aac5f4e84ac027cb3dc
jamo89 <jameson89@live.com>
2025-02-12 12:27:40 -0800

    rewrote a bunch of this and finally finished this part of the tutorial after adding npm install --save-dev @testing-library/react @testing-library/jest-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,13 @@
-const topDesserts = [
-  {
-    id: "1",
-    title: "Tiramisu",
-    description: "The best tiramisu",
-    image: "https://picsum.photos/200/300/?random",
-    price: "$5.00",
-  },
-  {
-    id: "2",
-    title: "Lemon Ice Cream",
-    description: "Mind Blowing Taste",
-    image: "https://picsum.photos/200/300/?random",
-    price: "$4.50"
-  },
-  {
-    id: "3",
-    title: "Chocolate Mousse",
-    description: "Unexplored Flavor",
-    image: "https://picsum.photos/200/300/?random",
-    price: "$6.00",
-  },
-];
+import "./App.css"
 
-function App() {
-  const listItems = topDesserts.map(dessert => {
-    console.log("DESSERT ",dessert)
-    const itemText = `${dessert.title} - ${dessert.price}`
-    return <li>{itemText}</li>
-  })
-   return (
-      <div>
-        <ul>
-          {listItems}
-        </ul>
+function App () {
+  return (
+      <div className="App">
+        <a href="https://littlelemon.com" className="App-link">
+          Little Lemon Restaurant
+        </a>
       </div>
   );
 }
-
-// const topDesserts = data.map(dessert => {
-//   return {
-//     content: `${dessert.title} - ${dessert.description} - ${dessert.image}`,
-//     price: dessert.price,
-//   }
-// })
 
 export default App;
